4edfed760be26227aa8acc5b801de4a72f1a242f
furiousdave <davebertrand@gmail.com>
2015-08-18 22:06:22 +0000

    Moved sort order migration

--- djangocms_blog/migrations/0010_blogcategory_sort_order.py
+++ djangocms_blog/migrations/0010_blogcategory_sort_order.py
@@ -2,13 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import djangocms_text_ckeditor.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djangocms_blog', '0007_auto_20150719_0933'),
+        ('djangocms_blog', '0009_latestpostsplugin_tags_new'),
     ]
 
     operations = [
